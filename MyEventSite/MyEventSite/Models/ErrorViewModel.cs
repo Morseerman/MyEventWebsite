36c17500292d097152150dfb544bfcb415305162
Morseerman <36846035+Morseerman@users.noreply.github.com>
2021-04-27 15:23:55 +0000

    Add files via upload

---
+++ MyEventSite/MyEventSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyEventSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
